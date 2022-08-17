bbba2733ebfab5f78c5edf7d7d59e58361de5ea7
bodynar <bodynar@gmail.com>
2022-08-17 12:34:51 +0300

    add sending test email component

--- src/modules/user/components/emailTest/index.tsx
+++ src/modules/user/components/emailTest/index.tsx
@@ -1,13 +1,83 @@
-type EmailTestProps = {
+import { useCallback, useState } from "react";
 
+import { connect } from "react-redux";
+
+import { isStringEmpty } from "@bodynarf/utils";
+
+import Button from "@bodynarf/react.components/components/button";
+import Text from "@bodynarf/react.components/components/primitives/text";
+
+import { sendTestEmail } from "@app/redux/user/thunks/sendTestEmail";
+
+/**
+ * Email match regex
+ * Provided by https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#basic_validation
+ */
+const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+type EmailTestProps = {
+    /** Send test email via api */
+    sendTestEmail: (recipient: string) => void;
 };
 
-const EmailTest = (props: EmailTestProps): JSX.Element => {
+/** Component providing small form to test email integration */
+const EmailTest = ({ sendTestEmail }: EmailTestProps): JSX.Element => {
+    const [value, setValue] = useState('');
+    const [isValid, setIsValid] = useState(false);
+
+    const onValueChange = useCallback(
+        (value: string) => {
+            setValue(value);
+            setIsValid(true);
+
+            if (isStringEmpty(value)) {
+                return;
+            }
+        }, []);
+
+    const onSendClick = useCallback(() => {
+        if (isStringEmpty(value)) {
+            return;
+        }
+
+        const isEmail = emailPattern.test(value);
+
+        setIsValid(isEmail);
+
+        if (!isEmail) {
+            return;
+        }
+
+        sendTestEmail(value);
+    }, [value, sendTestEmail]);
+
     return (
-        <>
-            EmailTest
-        </>
+        <div className="box">
+            <div className="mb-2">
+                <Text
+                    onValueChange={onValueChange}
+                    defaultValue={value}
+                    label={{ caption: 'Recipient' }}
+                    placeholder="Enter valid email of recipient"
+                />
+                {!isValid && value.length > 0 &&
+                    <p className="help is-danger">Recipient email must be filled by template email@example.com</p>
+                }
+            </div>
+
+            <Button
+                type="primary"
+                caption="Send"
+                onClick={onSendClick}
+                disabled={!isValid}
+            />
+        </div>
     );
 };
 
-export default EmailTest;
+export default connect(
+    _ => ({}),
+    {
+        sendTestEmail
+    }
+)(EmailTest);
---
+++ src/redux/user/thunks/sendTestEmail.ts
@@ -0,0 +1,30 @@
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
+
+import { post } from "@app/utils/delayedApi";
+
+import { CompositeAppState } from "@app/redux/rootReducer";
+import { Action } from "@app/redux/types";
+import { getDisplayErrorMessageAction, getDisplaySuccessMessageAction } from "@app/redux/utils";
+
+import { getSetAppIsLoadingAction } from "@app/redux/app/actions/setAppIsLoading";
+
+/**
+ * Send test email message
+ * @returns Action function that can be called with redux dispatcher
+ */
+export const sendTestEmail = (recipient: string): ThunkAction<void, CompositeAppState, unknown, Action> =>
+    async (dispatch: ThunkDispatch<CompositeAppState, unknown, Action>,
+        getState: () => CompositeAppState
+    ): Promise<void> => {
+        dispatch(getSetAppIsLoadingAction(true));
+
+        try {
+            await post<Array<string>>(`api/user/testMailMessage`, {
+                recipient: recipient
+            });
+            dispatch(getSetAppIsLoadingAction(false));
+            getDisplaySuccessMessageAction(dispatch, getState)('Message sucessfully added to queue');
+        } catch (error) {
+            getDisplayErrorMessageAction(dispatch, getState)(error as any);
+        }
+    };
