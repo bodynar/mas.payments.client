462e70834f5066ac516e46c7a75be56bdca5edd2
bodynar <bodynar@gmail.com>
2023-04-20 23:45:48 +0300

    add chart models

---
+++ src/shared/models/stats/chartConfig.ts
@@ -0,0 +1,20 @@
+import { SelectableItem } from "@bodynarf/react.components";
+
+import { LookupDate } from "@app/models";
+
+import { Chart } from ".";
+
+/** Chart config data */
+export interface ChartConfig {
+    /** Chart key */
+    chart: Chart;
+
+    /** Left date corridor border */
+    from: LookupDate;
+
+    /** Right date corridor border */
+    to: LookupDate;
+
+    /** Selected type to filter series */
+    type?: SelectableItem;
+}
---
+++ src/shared/models/stats/chartData.ts
@@ -0,0 +1,8 @@
+/** Chart data series */
+export interface ChartData {
+    /** Unique key */
+    key: number;
+
+    /** Values */
+    data: Map<string, number>;
+}
