7e665662b5dce209572fba701b477ee8d8ab37a8
bodynar <bodynar@gmail.com>
2023-04-23 01:44:47 +0300

    add payments chart module without chart

---
+++ src/modules/stats/components/chartDateOptions/index.tsx
@@ -0,0 +1,119 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+import { isNullOrUndefined } from "@bodynarf/utils";
+import { SelectableItem } from "@bodynarf/react.components";
+import Button from "@bodynarf/react.components/components/button";
+import Dropdown from "@bodynarf/react.components/components/dropdown";
+
+import { monthsAsDropdownItems, yearsAsDropdownItems } from "@app/constants";
+import { LookupDate } from "@app/models";
+
+import { getNowDate } from "@app/utils";
+
+/** Chart date options component props type */
+interface ChartDateOptionsProps {
+    /** Row date captiom */
+    caption: string;
+
+    /** Default selected values */
+    defaultValue?: LookupDate;
+
+    /** Value change handler */
+    onValueChage: (date: LookupDate) => void;
+}
+
+/** Chart date range border select component */
+const ChartDateOptions = ({
+    caption, onValueChage,
+    defaultValue,
+}: ChartDateOptionsProps): JSX.Element => {
+    const isFirstRun = useRef(true);
+    const [todayIsActive, setTodayIsActive] = useState(getDateIsToday(defaultValue));
+
+    const [date, setDate] = useState<LookupDate>(defaultValue ?? {});
+
+    useEffect(
+        () => {
+            if (isFirstRun.current) {
+                isFirstRun.current = false;
+                return;
+            }
+
+            onValueChage(date);
+
+            setTodayIsActive(
+                getDateIsToday(date)
+            );
+        },
+        [date, onValueChage, setTodayIsActive]
+    );
+
+    const onMonthSelect = useCallback((item?: SelectableItem) => setDate(x => ({ ...x, month: item })), []);
+    const onYearSelect = useCallback((item?: SelectableItem) => setDate(x => ({ ...x, year: item })), []);
+
+    const onTodayClick = useCallback(
+        () => {
+            const { month, year } = getNowDate();
+
+            setDate({
+                month: monthsAsDropdownItems().find(({ value }) => +value === month),
+                year: yearsAsDropdownItems().find(({ value }) => +value === year),
+            });
+        },
+        []
+    );
+
+    return (
+        <div className="columns">
+            <div className="column is-4">
+                <Dropdown
+                    placeholder="Month"
+                    value={date.month}
+                    deselectable={true}
+                    hideOnOuterClick={true}
+                    onSelect={onMonthSelect}
+                    items={monthsAsDropdownItems()}
+                    label={{
+                        caption: caption,
+                        horizontal: true,
+                    }}
+                />
+            </div>
+            <div className="column is-3">
+                <Dropdown
+                    placeholder="Year"
+                    value={date.year}
+                    deselectable={true}
+                    onSelect={onYearSelect}
+                    hideOnOuterClick={true}
+                    items={yearsAsDropdownItems()}
+                />
+            </div>
+            <div className="column">
+                <Button
+                    type="success"
+                    caption="Today"
+                    outlined={true}
+                    onClick={onTodayClick}
+                    disabled={!todayIsActive}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ChartDateOptions;
+
+/**
+ * Get is specified date describes current date (today)
+ * @param date Selected date by lookups
+ * @returns `true` if selected date is current date; otherwise - `false`
+ */
+const getDateIsToday = (date?: LookupDate): boolean => {
+    const { month, year } = getNowDate();
+
+    return isNullOrUndefined(date?.month) || isNullOrUndefined(date?.year)
+        ? true
+        : +date!.month!.value! !== month
+        || +date!.year!.value! !== year;
+};
--- src/modules/stats/components/payments/index.tsx
+++ src/modules/stats/components/payments/index.tsx
@@ -1,27 +1,149 @@
+import { useCallback, useEffect, useState } from "react";
+
 import { connect } from "react-redux";
 
-interface PaymentsChartProps {
+import { SelectableItem } from "@bodynarf/react.components";
+import Button from "@bodynarf/react.components/components/button";
+import Dropdown from "@bodynarf/react.components/components/dropdown";
 
+import { LookupDate } from "@app/models";
+import { Chart, ChartConfig, ChartData } from "@app/models/stats";
+
+import { CompositeAppState } from "@app/redux";
+import { loadTypes } from "@app/redux/payments";
+import { getSaveChartConfigAction, loadChartData } from "@app/redux/stats";
+
+import ChartDateOptions from "../chartDateOptions";
+
+/** Payments chart component props type */
+interface PaymentsChartProps {
+    /** Payment types mapped to dropdown items to cache values */
+    availableTypesAsDropdownItems: Array<SelectableItem>;
+
+    /** Last loaded chart data */
+    chartSeriesData: Array<ChartData>;
+
+    /** Last chart configuration */
+    lastConfig?: ChartConfig;
+
+    /** Load all payment types */
+    loadTypes: () => void;
+
+    /** Save current chart configuration */
+    saveConfig: (config: ChartConfig) => void;
+
+    /** Load chart series data by specified config */
+    loadChartData: (config: ChartConfig) => Promise<void>;
 }
 
+/** Payments chart component */
+const PaymentsChart = ({
+    availableTypesAsDropdownItems,
+    loadTypes, loadChartData, saveConfig,
+    chartSeriesData, lastConfig,
+}: PaymentsChartProps): JSX.Element => {
+    useEffect(() => {
+        if (availableTypesAsDropdownItems.length === 0) {
+            loadTypes();
+        }
+    }, [loadTypes, availableTypesAsDropdownItems]);
 
-const PaymentsChart = (_: PaymentsChartProps): JSX.Element => {
+    const [fromDate, setFromDate] = useState<LookupDate>(lastConfig?.from ?? {});
+    const [toDate, setToDate] = useState<LookupDate>(lastConfig?.to ?? {});
+    const [type, setType] = useState<SelectableItem | undefined>(lastConfig?.type);
 
+    const onShowDataClick = useCallback(
+        () =>
+            loadChartData({
+                chart: Chart.Payments,
+                from: fromDate,
+                to: toDate,
+                type: type,
+            }),
+        [fromDate, loadChartData, toDate, type]
+    );
+
+    useEffect(
+        () => {
+            saveConfig({
+                chart: Chart.Payments,
+                from: fromDate,
+                to: toDate,
+                type: type,
+            });
+        },
+        [fromDate, saveConfig, toDate, type]
+    );
+
+    if (availableTypesAsDropdownItems.length === 0) {
+        return <></>;
+    }
 
     return (
         <>
-            PaymentsChart
+            <nav>
+                <div className="notification is-warning is-light">
+                    To apply date as filter you should select <b>month and year</b>
+                </div>
+                <ChartDateOptions
+                    caption="From"
+                    onValueChage={setFromDate}
+                    defaultValue={lastConfig?.from}
+                />
+                <ChartDateOptions
+                    caption="To"
+                    onValueChage={setToDate}
+                    defaultValue={lastConfig?.to}
+                />
+                <div className="columns">
+                    <div className="column is-4">
+                        <Dropdown
+                            value={type}
+                            placeholder="Type"
+                            deselectable={true}
+                            onSelect={setType}
+                            hideOnOuterClick={true}
+                            items={availableTypesAsDropdownItems}
+                            label={{
+                                caption: "Type",
+                                horizontal: true,
+                            }}
+                        />
+                    </div>
+                </div>
+
+                <div className="columns">
+                    <div className="column">
+                        <Button
+                            type="primary"
+                            caption="Show data"
+                            onClick={onShowDataClick}
+                        />
+                    </div>
+                </div>
+            </nav>
+            <hr />
+            <section>
+                {chartSeriesData.length}
+            </section>
         </>
     );
 };
 
 export default
     connect(
-        () => { },
-        {
+        ({ payments, stats }: CompositeAppState) => {
+            const chartData = stats.charts.get(Chart.Payments);
 
+            return {
+                availableTypesAsDropdownItems: payments.availableTypesAsDropdownItems,
+                chartSeriesData: chartData?.lastData ?? [],
+                lastConfig: chartData?.lastConfig,
+            };
+        },
+        {
+            loadTypes: loadTypes,
+            loadChartData: loadChartData,
+            saveConfig: getSaveChartConfigAction,
         }
-    )(
-        PaymentsChart
-    )
-    ;
+    )(PaymentsChart);
