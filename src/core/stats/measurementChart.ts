eba46e53253d8e1818a73f854ff77804fa8f4a8f
bodynar <bodynar@gmail.com>
2023-04-23 02:32:08 +0300

    add measurement chart

--- src/core/stats/measurementChart.ts
+++ src/core/stats/measurementChart.ts
@@ -1,5 +1,55 @@
-import { ChartConfig, ChartData } from "@app/models/stats";
+import { isNullOrUndefined, isStringEmpty } from "@bodynarf/utils";
 
-export const getChartData = (config: ChartConfig): Promise<Array<ChartData>> => {
-    return undefined;
+import { ChartConfig, ChartData } from "@app/models/stats";
+import { getShortMonthName } from "@app/constants";
+import { getDateIfDefined, get } from "@app/utils";
+
+/**
+ * Load measurements chart series data
+ * @param config Chart configuration
+ * @returns Promise with chart series data
+ */
+export const getChartData = ({ from, to, type }: ChartConfig): Promise<Array<ChartData>> => {
+    const fromDate = getDateIfDefined(from);
+    const toDate = getDateIfDefined(to);
+
+    const queryParams = new URLSearchParams();
+
+    if (!isNullOrUndefined(fromDate)) {
+        queryParams.set("from", new Date(fromDate!.year, fromDate!.month).toDateString());
+    }
+    if (!isNullOrUndefined(toDate)) {
+        queryParams.set("to", new Date(toDate!.year, toDate!.month).toDateString());
+    }
+    if (!isNullOrUndefined(type)) {
+        queryParams.set("paymentTypeId", type!.value);
+    }
+
+    const queryString = queryParams.toString();
+
+    const apiUri = isStringEmpty(queryString)
+        ? "api/stats/getMeasurementStatistics"
+        : `api/stats/getMeasurementStatistics?${queryString}`;
+
+    return get<Array<any>>(apiUri)
+        .then(({ typeStatistics }: any) => {
+            const isSameYear = new Set(
+                typeStatistics.map((x: any) => x['statisticsData'].map((y: any) => y['year'])).flat()
+            ).size === 1;
+
+            return typeStatistics.map((x: any) => ({
+                key: x.measurementTypeName,
+                data: new Map<string, number>(
+                    x['statisticsData'].map(
+                        (y: any) =>
+                            [
+                                isSameYear
+                                    ? getShortMonthName(y['month'])
+                                    : getShortMonthName(y['month']) + ' ' + y['year'],
+                                y['measurementDiff']
+                            ]
+                    )
+                )
+            }) as ChartData);
+        });
 };
--- src/modules/stats/components/measurements/index.tsx
+++ src/modules/stats/components/measurements/index.tsx
@@ -1,27 +1,140 @@
+import { useCallback, useEffect, useState } from "react";
+
 import { connect } from "react-redux";
 
-interface MeasurementsChartProps {
+import { SelectableItem } from "@bodynarf/react.components";
+import Button from "@bodynarf/react.components/components/button";
+import Dropdown from "@bodynarf/react.components/components/dropdown";
+import Icon from "@bodynarf/react.components/components/icon";
 
-}
+import { LookupDate } from "@app/models";
+import { Chart } from "@app/models/stats";
 
+import { CompositeAppState } from "@app/redux";
+import { loadTypes } from "@app/redux/measurements";
+import { getSaveChartConfigAction, loadChartData } from "@app/redux/stats";
 
-const MeasurementsChart = (_: MeasurementsChartProps): JSX.Element => {
+import ChartDateOptions from "../chartDateOptions";
+import { ChartComponentProps } from "../types";
 
+/** Payments chart component props type */
+interface MeasurementsChartProps extends ChartComponentProps { }
+
+/** Payments chart component */
+const MeasurementsChart = ({
+    availableTypesAsDropdownItems,
+    loadTypes, loadChartData, saveConfig,
+    chartSeriesData, lastConfig,
+}: MeasurementsChartProps): JSX.Element => {
+    useEffect(() => {
+        if (availableTypesAsDropdownItems.length === 0) {
+            loadTypes();
+        }
+    }, [loadTypes, availableTypesAsDropdownItems]);
+
+    const [fromDate, setFromDate] = useState<LookupDate>(lastConfig?.from ?? {});
+    const [toDate, setToDate] = useState<LookupDate>(lastConfig?.to ?? {});
+    const [type, setType] = useState<SelectableItem | undefined>(lastConfig?.type);
+
+    const onShowDataClick = useCallback(
+        () =>
+            loadChartData({
+                chart: Chart.Measurements,
+                from: fromDate,
+                to: toDate,
+                type: type,
+            }),
+        [fromDate, loadChartData, toDate, type]
+    );
+
+    useEffect(
+        () => {
+            saveConfig({
+                chart: Chart.Measurements,
+                from: fromDate,
+                to: toDate,
+                type: type,
+            });
+        },
+        [fromDate, saveConfig, toDate, type]
+    );
+
+    if (availableTypesAsDropdownItems.length === 0) {
+        return <></>;
+    }
 
     return (
         <>
-            MeasurementsChart
+            <nav className="block">
+                <div className="block is-italic	">
+                    <p>
+                        <Icon name="question-circle" className="mr-1"/>
+                        Displays change of measurement difference through time
+                    </p>
+                </div>
+                <div className="notification is-warning is-light">
+                    To apply date as filter you should select <b>month and year</b>
+                </div>
+
+                <ChartDateOptions
+                    caption="From"
+                    onValueChage={setFromDate}
+                    defaultValue={lastConfig?.from}
+                />
+                <ChartDateOptions
+                    caption="To"
+                    onValueChage={setToDate}
+                    defaultValue={lastConfig?.to}
+                />
+                <div className="columns">
+                    <div className="column is-4">
+                        <Dropdown
+                            value={type}
+                            placeholder="Type"
+                            deselectable={true}
+                            onSelect={setType}
+                            hideOnOuterClick={true}
+                            items={availableTypesAsDropdownItems}
+                            label={{
+                                caption: "Type",
+                                horizontal: true,
+                            }}
+                        />
+                    </div>
+                </div>
+
+                <div className="columns">
+                    <div className="column">
+                        <Button
+                            type="primary"
+                            caption="Show data"
+                            onClick={onShowDataClick}
+                        />
+                    </div>
+                </div>
+            </nav>
+            <hr />
+            <section>
+                {chartSeriesData.length}
+            </section>
         </>
     );
 };
 
 export default
     connect(
-        () => { },
-        {
+        ({ measurements, stats }: CompositeAppState) => {
+            const chartData = stats.charts.get(Chart.Measurements);
 
+            return {
+                availableTypesAsDropdownItems: measurements.availableTypesAsDropdownItems,
+                chartSeriesData: chartData?.lastData ?? [],
+                lastConfig: chartData?.lastConfig,
+            };
+        },
+        {
+            loadTypes: loadTypes,
+            loadChartData: loadChartData,
+            saveConfig: getSaveChartConfigAction,
         }
-    )(
-        MeasurementsChart
-    )
-    ;
+    )(MeasurementsChart);
